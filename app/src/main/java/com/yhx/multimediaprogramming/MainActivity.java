3c2965d877648d882dea42674eae8e5443150e93
sunshineCEO <1298435317@qq.com>
2017-11-04 15:37:25 +0800

    加载大图片

--- app/src/main/java/com/yhx/multimediaprogramming/MainActivity.java
+++ app/src/main/java/com/yhx/multimediaprogramming/MainActivity.java
@@ -1,7 +1,11 @@
 package com.yhx.multimediaprogramming;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void click(View view) {
+        //加载图片
+        Bitmap bm = BitmapFactory.decodeFile("sdcard/dog.jpg");
+        ImageView iv = (ImageView) findViewById(R.id.iv);
+        iv.setImageBitmap(bm);
     }
 }
